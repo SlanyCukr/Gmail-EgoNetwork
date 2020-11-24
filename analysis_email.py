e30401feaa4bec110bb55425dbfaeadf68fe73c1
slanycukr <hovadik@atlasgroup.cz>
2020-11-24 21:59:07 +0100

    Using try-except in all scripts

--- analysis_email.py
+++ analysis_email.py
@@ -7,24 +7,27 @@ unique_nodes = {}
 
 # read lines from fetched emails
 for line in f:
-    parts = f.readline().split(';')
+    try:
+        parts = f.readline().split(';')
 
-    if '<' in parts[1] and '>' in parts[1]:
-        sender = re.search('<(.*)>', parts[1]).group(1).lower()
-    else:
-        sender = parts[1].lower()
+        if '<' in parts[1] and '>' in parts[1]:
+            sender = re.search('<(.*)>', parts[1]).group(1).lower()
+        else:
+            sender = parts[1].lower()
 
-    if '<' in parts[2] and '>' in parts[2]:
-        recipient = re.search('<(.*)>', parts[2]).group(1).lower()
-    else:
-        recipient = parts[2].lower()
+        if '<' in parts[2] and '>' in parts[2]:
+            recipient = re.search('<(.*)>', parts[2]).group(1).lower()
+        else:
+            recipient = parts[2].lower()
 
-    if sender not in unique_nodes:
-        unique_nodes[sender] = len(unique_nodes)
-    if recipient not in unique_nodes:
-        unique_nodes[recipient] = len(unique_nodes)
+        if sender not in unique_nodes:
+            unique_nodes[sender] = len(unique_nodes)
+        if recipient not in unique_nodes:
+            unique_nodes[recipient] = len(unique_nodes)
 
-    lines_to_write.append(f"{unique_nodes[sender]};{unique_nodes[recipient]}")
+        lines_to_write.append(f"{unique_nodes[sender]};{unique_nodes[recipient]}")
+    except:
+        pass
 
 f.close()
 
--- analysis_subject.py
+++ analysis_subject.py
@@ -5,18 +5,21 @@ unique_nodes = {}
 
 # read lines from fetched emails
 for line in f:
-    parts = f.readline().split(';')
+    try:
+        parts = f.readline().split(';')
 
-    words = parts[4].split(' ')
+        words = parts[4].split(' ')
 
-    for word in words:
-        if word not in unique_nodes:
-            unique_nodes[word] = len(unique_nodes)
+        for word in words:
+            if word not in unique_nodes:
+                unique_nodes[word] = len(unique_nodes)
 
-    # inspired by GetCombination method in Jakub Plesnik's C# hint of solution
-    for i in range(len(words) - 1):
-        for j in range(i + 1, len(words)):
-            lines_to_write.append(f"{unique_nodes[words[i]]};{unique_nodes[words[j]]}")
+        # inspired by GetCombination method in Jakub Plesnik's C# hint of solution
+        for i in range(len(words) - 1):
+            for j in range(i + 1, len(words)):
+                lines_to_write.append(f"{unique_nodes[words[i]]};{unique_nodes[words[j]]}")
+    except:
+        pass
 
 f.close()
 
