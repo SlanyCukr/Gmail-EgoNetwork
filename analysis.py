44959cda40c492f99f715b7d789b193ad542f5fc
slanycukr <hovadik@atlasgroup.cz>
2020-11-24 10:19:03 +0100

    Create edges with recipients, senders

--- analysis.py
+++ analysis.py
@@ -2,8 +2,21 @@ lines_to_write = []
 
 f = open("output.csv", "r")
 
+unique_nodes = {}
+nodes_to_export = []
 
 for line in f:
     parts = f.readline().split(';')
+
+    sender = parts[1]
+    recipient = parts[2]
+    subject = parts[4]
+
+    if sender not in unique_nodes:
+        unique_nodes[sender] = len(unique_nodes)
+    if recipient not in unique_nodes:
+        unique_nodes[recipient] = len(unique_nodes)
+
+    lines_to_write.append(f"{unique_nodes[sender]}:{unique_nodes[recipient]}")
 
 f.close()
